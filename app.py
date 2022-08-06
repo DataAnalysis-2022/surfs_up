584cb28dc30213e4021673283dc41e62d7610271
DataAnalysis-2022 <106888594+DataAnalysis-2022@users.noreply.github.com>
2022-08-06 12:34:46 -0400

    New Files

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def temp_monthly():
     return jsonify(temps=temps)
 
 
-
+@app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
